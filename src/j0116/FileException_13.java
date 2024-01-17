0ce7fe56893aa9261e91a237c27d4a7fcf55580d
song <jjangsgw@gmail.com>
2024-01-17 14:56:52 +0900

    11

---
+++ src/j0116/FileException_13.java
@@ -0,0 +1,18 @@
+package j0116;
+//파일 입출력
+/*파일 읽기 _ FileReader
+ * 파일 복사 _ FileWriter
+ * 파일 쓰기 _ BufferedReader
+ * 폴더 내 파일 읽기 _ BufferedWriter
+ * 
+ * java 기본 기능으로 파일 내용을 읽고, 새로운 파일에 쓴느 방법!!!!
+ */
+
+public class FileException_13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
